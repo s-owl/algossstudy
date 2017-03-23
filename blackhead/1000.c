8dfbde4695fab396ef6e87282254592bad374842
BlackHead <bruce2052@gmail.com>
2017-03-23 19:12:50 +0900

    Add files via upload
    1000문제

---
+++ blackhead/1000.c
@@ -0,0 +1,16 @@
+﻿//201132003 공원배
+//알고리즘 1000번 문제
+//사용언어 : C
+
+#include <stdio.h>
+
+int main(){
+    
+    int a,b;
+    int sum = 0;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    sum = a+b;
+    printf("%d", sum);
+    return 0;
+}
