19604eda16bb28a519044de5e454073074b226f7
chugeonwoo <cnrjsdn@gmail.com>
2017-03-24 23:40:24 +0900

    chu > solve 10951

---
+++ chu/10951.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class a10951 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNextInt()){
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}
