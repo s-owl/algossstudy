eee1b2b51057e58ea78cb1979aa303ac479c0200
harvey <vaporize93@gmail.com>
2017-03-30 20:21:14 +0900

    문제풀이 결과 확인용 커맨드 작성

---
+++ util/check_hw_online.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+USER = ['vaporize93', 'doo9713', '21pori30', 'blackhead', 'cnrjsdn',]
+WEEK = {
+        1: [2557, 1000, 2558, 10950, 10951, 
+            10953, 11021, 11022, 11718, 11719, 11720, 11721],
+        }
+
+problem = {}
+
+for u in USER:
+    html_doc = requests.get('https://www.acmicpc.net/user/'+ u).text
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    solvs = soup.find('div', class_='panel-body').find_all('span', class_='problem_number')
+    problem[u] = []
+
+    for solv in solvs:
+        num = solv.a.string
+        problem[u].append(num)
+        
+
+for i in WEEK.keys():
+    print(f'==== {i}주차 아직 못 푼 문제 ====')
+    for (u, ps) in problem.items():
+        l = [x for x in WEEK[i] if x not in list(map(int, ps))]
+        if l:
+            print(f' - {u} {l}')
